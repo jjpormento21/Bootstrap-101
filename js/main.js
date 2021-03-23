235e230f144d8a176a8cf478392554bda0041b9e
jjpormento21 <jonahjamespormento21@gmail.com>
2021-03-23 12:50:53 +0800

    prototype forms

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
+ if(dd<10){
+        dd='0'+dd
+    } 
+    if(mm<10){
+        mm='0'+mm
+    } 
+
+today = mm+'-'+dd+'-'+yyyy;
+document.getElementById("datePicker").setAttribute("max", today);
